12c9f45ac4d297401cf0733da2c1ad39203a261b
Shubham <shubhamkharche2003@gmail.com>
2025-07-28 19:56:34 +0530

    Recruitra setup completed

---
+++ src/main/java/com/Shubham/Recruitra/RecruitraApplication.java
@@ -0,0 +1,13 @@
+package com.Shubham.Recruitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shubham/Recruitra/RecruitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shubham.Recruitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
