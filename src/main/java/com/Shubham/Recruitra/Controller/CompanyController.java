3b1d57c8e99a733642edac2ca0c5d086ed8e5e84
Shubham <shubhamkharche2003@gmail.com>
2025-07-29 12:21:58 +0530

    CompanyService and CompanyContrller completed

--- src/main/java/com/Shubham/Recruitra/Controller/CompanyController.java
+++ src/main/java/com/Shubham/Recruitra/Controller/CompanyController.java
@@ -1,4 +1,46 @@
 package com.Shubham.Recruitra.Controller;
 
+import com.Shubham.Recruitra.Dto.CompanyDto;
+import com.Shubham.Recruitra.Service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/company")
 public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<CompanyDto>> getAllCompany(
+            @RequestParam(value = "pageNum", defaultValue = "3") int pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "1") int pageSize,
+            @RequestParam(defaultValue = "companyId") String sortBy
+    ) {
+        return new ResponseEntity<>(companyService.getAllCompanyData(pageNum - 1, pageSize, sortBy), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/byId/{companyId}")
+    public ResponseEntity<CompanyDto> getCompanyById(@PathVariable Long companyId){
+        return new ResponseEntity<>(companyService.getCompanyDataById(companyId),HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> createCompany(@RequestBody CompanyDto companyDto){
+        return new ResponseEntity<>(companyService.createCompanyData(companyDto),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/put/{companyId}")
+    public ResponseEntity<String> updateCompany(@PathVariable Long companyId,@RequestBody CompanyDto companyDto){
+        return new ResponseEntity<>(companyService.updateCompanyData(companyId,companyDto),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{companyId}")
+    public ResponseEntity<String> deleteCompany(@PathVariable Long companyId){
+        return new ResponseEntity<>(companyService.deleteCompany(companyId),HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/Shubham/Recruitra/Service/CompanyService.java
+++ src/main/java/com/Shubham/Recruitra/Service/CompanyService.java
@@ -1,7 +1,87 @@
 package com.Shubham.Recruitra.Service;
 
+import com.Shubham.Recruitra.Dto.CompanyDto;
+import com.Shubham.Recruitra.Entity.Company;
+import com.Shubham.Recruitra.Exception.CompanyExistException;
+import com.Shubham.Recruitra.Exception.CompanyNotFoundException;
+import com.Shubham.Recruitra.Repository.CompanyRepo;
+import com.Shubham.Recruitra.Service.Email.EmailService;
+import jakarta.mail.MessagingException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
+
+    @Autowired
+    private CompanyRepo companyRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private EmailService emailService;
+
+    public Page<CompanyDto> getAllCompanyData(int pageNum, int pageSize, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize, Sort.by(sortBy));
+        Page<Company> companyPage = companyRepo.findAll(pageable);
+        return companyPage.map(Company -> modelMapper.map(Company, CompanyDto.class));
+    }
+
+    public CompanyDto getCompanyDataById(Long companyId) {
+        Company company = companyRepo.findById(companyId)
+                .orElseThrow(() -> new CompanyNotFoundException("Company does not exist!"));
+        return modelMapper.map(company, CompanyDto.class);
+    }
+
+    public String createCompanyData(CompanyDto companyDto){
+        if(companyRepo.existsByCompanyName(companyDto.getCompanyName())){
+            throw new CompanyExistException("Company already registered!");
+        }
+        Company company = modelMapper.map(companyDto,Company.class);
+        companyRepo.save(company);
+        try{
+            emailService.sendWelcomeEmailToCompany(companyDto.getCompanyEmail(),companyDto.getCompanyName());
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return "Company registered successfully!";
+    }
+
+    public String updateCompanyData(Long companyId,CompanyDto companyDto){
+        Company company = companyRepo.findById(companyId)
+                .orElseThrow(()->new CompanyNotFoundException("Company does not exist!"));
+        if(companyDto.getCompanyName()!=null){
+            company.setCompanyName(companyDto.getCompanyName());
+        }
+        if(companyDto.getCompanyIndustry()!=null){
+            company.setCompanyIndustry(companyDto.companyIndustry);
+        }
+        if(companyDto.getCompanyLocation()!=null){
+            company.setCompanyLocation(companyDto.getCompanyLocation());
+        }
+        if(companyDto.getCompanyWebsite()!=null){
+            company.setCompanyWebsite(companyDto.getCompanyWebsite());
+        }
+        if(companyDto.getCompanyEmail()!=null){
+            company.setCompanyEmail(companyDto.getCompanyEmail());
+        }
+        if(companyDto.getContactNum()!=null){
+            company.setContactNum(companyDto.getContactNum());
+        }
+        companyRepo.save(company);
+        return "Company data updated successfully!";
+    }
+
+    public String deleteCompany(Long companyId){
+        Company company = companyRepo.findById(companyId)
+                .orElseThrow(()->new CompanyNotFoundException("Company does not exist!"));
+        companyRepo.deleteById(companyId);
+        return "Company data deleted successfully!";
+    }
 }
