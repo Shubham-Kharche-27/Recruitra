f37a502b11083696fbd34562710d0484259eac73
Shubham <shubhamkharche2003@gmail.com>
2025-07-29 18:31:16 +0530

    ApplicantService and ApplicantController completed!

--- src/main/java/com/Shubham/Recruitra/Controller/ApplicantController.java
+++ src/main/java/com/Shubham/Recruitra/Controller/ApplicantController.java
@@ -1,4 +1,46 @@
 package com.Shubham.Recruitra.Controller;
 
+import com.Shubham.Recruitra.Dto.ApplicantDto;
+import com.Shubham.Recruitra.Service.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/applicant")
 public class ApplicantController {
+
+    @Autowired
+    private ApplicantService applicantService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<ApplicantDto>> getAllApplicantData(
+            @RequestParam(value = "pageNum",defaultValue = "1")int pageNum,
+            @RequestParam(value = "pageSize",defaultValue = "3")int pageSize,
+            @RequestParam(defaultValue = "applicantId")String sortBy
+    ){
+        return new ResponseEntity<>(applicantService.getAllApplicantData(pageNum-1,pageSize,sortBy), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/byId/{applicantId}")
+    public ResponseEntity<ApplicantDto> getApplicantDataById(@PathVariable Long applicantId){
+        return new ResponseEntity<>(applicantService.getApplicantById(applicantId),HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> createApplicant(@RequestBody ApplicantDto applicantDto){
+        return new ResponseEntity<>(applicantService.createApplicantData(applicantDto),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/put/{applicantId}")
+    public ResponseEntity<String> updateApplicant(@PathVariable Long applicantId,@RequestBody ApplicantDto applicantDto){
+        return new ResponseEntity<>(applicantService.updateApplicantData(applicantId,applicantDto),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{applicantId}")
+    public ResponseEntity<String> deleteApplicant(@PathVariable Long applicantId){
+        return new ResponseEntity<>(applicantService.deleteApplicantData(applicantId),HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/Shubham/Recruitra/Service/ApplicantService.java
+++ src/main/java/com/Shubham/Recruitra/Service/ApplicantService.java
@@ -1,4 +1,88 @@
 package com.Shubham.Recruitra.Service;
 
+import com.Shubham.Recruitra.Dto.ApplicantDto;
+import com.Shubham.Recruitra.Entity.Applicant;
+import com.Shubham.Recruitra.Exception.ApplicantExistException;
+import com.Shubham.Recruitra.Exception.ApplicantNotFoundException;
+import com.Shubham.Recruitra.Repository.ApplicantRepo;
+import com.Shubham.Recruitra.Service.Email.EmailService;
+import jakarta.mail.MessagingException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ApplicantService {
+
+    @Autowired
+    private ApplicantRepo applicantRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private EmailService emailService;
+
+    public Page<ApplicantDto> getAllApplicantData(int pageNum, int pageSize, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize, Sort.by(sortBy));
+        Page<Applicant> applicantPage = applicantRepo.findAll(pageable);
+        return applicantPage.map(Applicant -> modelMapper.map(Applicant, ApplicantDto.class));
+    }
+
+    public ApplicantDto getApplicantById(Long applicantId) {
+        Applicant applicant = applicantRepo.findById(applicantId)
+                .orElseThrow(() -> new ApplicantNotFoundException("Applicant does not exist!"));
+        return modelMapper.map(applicant,ApplicantDto.class);
+    }
+
+    public String createApplicantData(ApplicantDto applicantDto){
+        if(applicantRepo.existsByApplicantFullName(applicantDto.getApplicantFullName())){
+            throw new ApplicantExistException("Applicant already registered!");
+        }
+        Applicant applicant = modelMapper.map(applicantDto,Applicant.class);
+        applicantRepo.save(applicant);
+        try{
+            emailService.sendWelcomeEmailToApplicant(applicantDto.getApplicantEmail(),applicantDto.getApplicantFullName());
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return "Applicant created successfully!";
+    }
+
+    public String updateApplicantData(Long applicantId,ApplicantDto applicantDto){
+        Applicant applicant = applicantRepo.findById(applicantId)
+                .orElseThrow(()->new ApplicantNotFoundException("Applicant does not exists!"));
+        if(applicantDto.getApplicantFullName()!=null){
+            applicant.setApplicantFullName(applicantDto.getApplicantFullName());
+        }
+        if(applicantDto.getApplicantEmail()!=null){
+            applicant.setApplicantEmail(applicantDto.getApplicantEmail());
+        }
+        if(applicantDto.getApplicantPhoneNum()!=null){
+            applicant.setApplicantFullName(applicantDto.getApplicantPhoneNum());
+        }
+        if(applicantDto.getApplicantResumeLink()!=null){
+            applicant.setApplicantResumeLink(applicantDto.getApplicantResumeLink());
+        }
+        if(applicantDto.getProfileSummary()!=null){
+            applicant.setProfileSummary(applicantDto.getProfileSummary());
+        }
+        if(applicantDto.getApplicantSkills()!=null){
+            applicant.setApplicantSkills(applicantDto.getApplicantSkills());
+        }
+        applicantRepo.save(applicant);
+        return "Applicant data updated successfully!";
+    }
+
+    public String deleteApplicantData(Long applicantId){
+        Applicant applicant = applicantRepo.findById(applicantId)
+                .orElseThrow(()->new ApplicantNotFoundException("Applicant does not exists!"));
+        applicantRepo.deleteById(applicantId);
+        return "Applicant deleted successfully!";
+    }
+
 }
